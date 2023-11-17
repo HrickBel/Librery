1b70aef6f6e15aa6f9520a394792941b3d12bf2d
Henrique Belém <115942716+HrickBel@users.noreply.github.com>
2023-11-17 16:56:13 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,5 +1,4 @@
 const {Sequelize} = require('sequelize');
-<<<<<<< HEAD
 const config = require('./database.config');
 const database = new Sequelize(
 	config.database,
@@ -8,28 +7,11 @@ const database = new Sequelize(
 	{
 		host:config.HOST,
 		dialect:'mysql'
-=======
-const database = new Sequelize(
-	'database',
-	'user',
-	'password',{
-		dialect:'mysql',
-		host:''
->>>>>>> cbe12c719810348b9ea24a994fb64c24cbab87e0
 	});
 
 database.authenticate().then(() => {
 	console.log('Banco de dados conectado');
-<<<<<<< HEAD
-}).catch((err) => {
-	console.error('não conectado:\n');
-});
-
-module.exports = database;
-=======
-
-	module.exports = database;
 }).catch((err) => {
 	console.error('não conectado:');
 });
->>>>>>> cbe12c719810348b9ea24a994fb64c24cbab87e0
+module.exports = database;
