4b2d68c9c3e6c1caaae80aa8251366027c375d00
Henrique Belém <115942716+HrickBel@users.noreply.github.com>
2023-12-06 17:43:42 +0000

    version 1.1

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('dotenv').config();
+console.log(process.env);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var cad_aln = require('./routes/cad.aln');
+var AddBook = require('./routes/cad.book');
 var AdmPanel = require('./routes/AdmPanel');
 
 var app = express();
@@ -33,6 +35,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/cad_aln', cad_aln);
 app.use('/AdmPanel',AdmPanel);
+app.use('/cad_book',AddBook);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- database/database.config.js
+++ database/database.config.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 module.exports = {
+	host: process.env.HOST,
 	database: process.env.DATABASE,
-	user: process.env.USER,
-	password: process.env.PASSWORD,
-	host: process.env.HOST
-};
+	user: process.env.USERDB,
+	password: process.env.PASSWORD
+}
